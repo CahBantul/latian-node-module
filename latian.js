e1cc8d0857bc25953ee888886bb5ac6c77a487be
Fardan Nozami <fardannozami@gmail.com>
2021-05-08 14:23:33 +0700

    add files

---
+++ latian.js
@@ -0,0 +1,12 @@
+// core module
+const os = require("os");
+const fs = require("fs");
+
+let cpuSpec = os.cpus();
+cpuSpecJSON = JSON.stringify(cpuSpec, null, 4);
+
+// write to file cpu.json
+fs.writeFile("cpu2.json", cpuSpecJSON, (err) => {
+  if (err) throw err;
+  console.log(`File berhasil di simpan`);
+});
---
+++ objectAssign.js
@@ -0,0 +1,13 @@
+// core module
+const os = require("os");
+const fs = require("fs");
+
+let cpuSpec = os.cpus();
+let cpuSpecObj = Object.assign({}, cpuSpec);
+let cpuSpecJSON = JSON.stringify(cpuSpecObj, null, 4);
+
+// write to file cpu.json
+fs.writeFile("cpu.json", cpuSpecJSON, (err) => {
+  if (err) throw err;
+  console.log(`File berhasil di simpan`);
+});
